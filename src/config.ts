b29c2683ca09fc615ad88d603e4c369acc3e349f
zwl <zwlong6@qq.com>
2025-08-25 00:02:23 +0800

    更新内容

--- src/config.ts
+++ src/config.ts
@@ -49,8 +49,10 @@ export const siteConfig: SiteConfig = {
 			], // 桌面横幅图片
 			mobile: [
 				// "assets/mobile-banner/1.png",
-				"assets/mobile-banner/3.webp",
 				// "https://s1.imagehub.cc/images/2025/08/24/3c8dd5aa69b67ffd8e89db3a93f670c6.webp",
+				// "assets/mobile-banner/3.webp",
+				"http://www.98qy.com/sjbz/api.php?=1"
+
 			], // 移动横幅图片
 		}, // 使用本地横幅图片
 
