db47788998437605c59c3e2c5711402d6fe920b9
zwl <zwlong6@qq.com>
2025-08-25 13:51:39 +0800

    增加twikoo

--- src/config.ts
+++ src/config.ts
@@ -203,7 +203,7 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 export const commentConfig: CommentConfig = {
 	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
 	twikoo: {
-		envId: "https://twikoo.zzzero.site",
+		envId: "https://twikoo1.zzzero.site",
 	},
 };
 
